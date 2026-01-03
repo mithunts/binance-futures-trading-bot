026f834da1d908dc4027ea81275f5b708b18f1ea
Mithun T S <202958120+mithunts@users.noreply.github.com>
2026-01-03 20:49:58 +0530

    Update grid.py

--- grid.py
+++ grid.py
@@ -1,1 +1,11 @@
 Grid stratergy
+
+from logger import logger
+
+def grid_trading(bot, symbol, side, start_price, grid_size, levels, quantity):
+    logger.info("Starting Grid Trading Strategy")
+
+    for i in range(levels):
+        price = start_price + (i * grid_size)
+        bot.place_limit_order(symbol, side, quantity, round(price, 2))
+        logger.info(f"Grid order placed at {price}")
