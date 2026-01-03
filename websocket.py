0c9a0937e011fb3a76e744cd1d89af24cad3692b
Mithun T S <202958120+mithunts@users.noreply.github.com>
2026-01-03 20:53:17 +0530

    Update websocket.py

--- websocket.py
+++ websocket.py
@@ -1,1 +1,15 @@
 WebSocket code
+
+from binance import ThreadedWebsocketManager
+from logger import logger
+
+def start_price_stream(symbol="btcusdt"):
+    twm = ThreadedWebsocketManager()
+    twm.start()
+
+    def handle(msg):
+        price = msg.get("c")
+        logger.info(f"Live price {symbol}: {price}")
+        print(f"Live price {symbol}: {price}")
+
+    twm.start_symbol_ticker_socket(symbol=symbol, callback=handle)
