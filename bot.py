ae21b2f806bef10470b80bdc087a27e27e91e23f
Mithun T S <202958120+mithunts@users.noreply.github.com>
2026-01-03 18:51:20 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,16 @@
+binance-futures-trading-bot/
+│
+├── bot.py
+├── cli.py
+├── websocket.py
+├── twap.py
+├── grid.py
+├── pnl.py
+├── risk.py
+├── logger.py
+├── config.py
+├── test_bot.py
+├── requirements.txt
+├── README.md
+└── logs/
+    └── trading_bot.log
