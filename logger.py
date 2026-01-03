825259df0c6970dc77c45fe3cc1159df2e628782
Mithun T S <202958120+mithunts@users.noreply.github.com>
2026-01-03 20:43:39 +0530

    Update logger.py

--- logger.py
+++ logger.py
@@ -1,1 +1,14 @@
 Logging setup
+
+import logging
+import os
+
+os.makedirs("logs", exist_ok=True)
+
+logging.basicConfig(
+    filename="logs/trading_bot.log",
+    level=logging.INFO,
+    format="%(asctime)s | %(levelname)s | %(message)s"
+)
+
+logger = logging.getLogger()
