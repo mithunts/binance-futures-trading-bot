9aa9843cc37a9a307e162bb5cbe08cd1794e26e2
Mithun T S <202958120+mithunts@users.noreply.github.com>
2026-01-03 19:04:14 +0530

    Update bot.py
    Update bot logic

--- bot.py
+++ bot.py
@@ -1,16 +1,1 @@
-binance-futures-trading-bot/
-│
-├── bot.py
-├── cli.py
-├── websocket.py
-├── twap.py
-├── grid.py
-├── pnl.py
-├── risk.py
-├── logger.py
-├── config.py
-├── test_bot.py
-├── requirements.txt
-├── README.md
-└── logs/
-    └── trading_bot.log
+BasicBot class
